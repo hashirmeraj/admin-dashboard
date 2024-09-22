7c09a2733b61bea67a667cc13570d63ae7d448d5
HASHIR MERAJ <hashirmeraj1@gmail.com>
2024-09-22 20:19:43 +0500

    Update card.js

--- js/card.js
+++ js/card.js
@@ -1,4 +1,6 @@
 var card = (function (modules) {
+
+    
     var installedModules = {}; function __webpack_require__(moduleId) {
         if (installedModules[moduleId])
             return installedModules[moduleId].exports; var module = installedModules[moduleId] = { exports: {}, id: moduleId, loaded: false }; modules[moduleId].call(module.exports, module, module.exports, __webpack_require__); module.loaded = true; return module.exports;
